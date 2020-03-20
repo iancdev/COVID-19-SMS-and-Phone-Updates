5bb21832491210319c02975b108e79e2ec4c62e8
iancdev <me@ian-chan.me>
2020-03-19 18:12:00 -0700

    Add files via upload

---
+++ covid19.py
@@ -0,0 +1,141 @@
+__author__ = "Ian Chan"
+__copyright__ = "Copyright 2020, Ian Chan"
+__credits__ = ["Ian Chan"]
+__license__ = "GPL v3"
+__version__ = "1"
+__maintainer__ = "Ian Chan"
+__email__ = "me@ian-chan.me"
+__status__ = "Production"
+
+import json, requests #import
+
+def messageFormat(composedMessage, inputType, compare = False, data = 'data', oldData = 'oldData', newData = 'newData'):
+    if inputType == 'message':
+        composedMessage = composedMessage.replace("{}", "\n")
+        composedMessage = composedMessage.replace("{linebreak}", "\n\n")
+        composedMessage = composedMessage.replace("{customizeBegin}", "")
+        composedMessage = composedMessage.replace("{customizeEnd}", "")
+    elif inputType == 'page':
+        composedMessage = composedMessage.replace("{}", "</br>")
+        composedMessage = composedMessage.replace("{linebreak}", "</br></br>")
+        composedMessage = composedMessage.replace("{customizeBegin}", "")
+        composedMessage = composedMessage.replace("{customizeEnd}", "")
+    elif inputType == 'call':
+        composedMessage = composedMessage.replace("{}", ". ")
+        composedMessage = composedMessage.replace("{linebreak}", ". ")
+        composedMessage = composedMessage.replace("{customizeBegin}", "Latest ")
+        composedMessage = composedMessage.replace("{customizeEnd}", ". Thank you for calling.")
+    elif inputType == 'raw':
+        composedMessage = data
+    if (compare == True) and (oldData == newData):
+        composedMessage = 'No Change'
+    else:
+        pass
+    return composedMessage
+
+def getData(inputType, country = 'us', compare = False, altAPI = False):
+    restore = False
+    if compare == True:
+        try:
+            with open(country, "r") as readFile:
+                oldData = readFile.read()
+        except:
+            oldData = 'None'
+    else:
+        pass
+    #set url
+    if altAPI == True: 
+        url = 'https://corona.lmao.ninja/countries/' + country
+    else:
+        url = 'https://corona.lmao.ninja/countries/' + country
+    r = requests.get(url)
+    with open(country, 'r+') as src, open(country + '.bak', 'w+') as dst:
+            dst.write(src.read()) #Backup
+    with open(country, 'wb') as outfile:
+        outfile.write(r.content) #Get latest and save
+    with open(country, "r+") as dataFile:
+        data = dataFile.read() #sets data to latest data
+        if data == 'Country not found': #if not available
+            print("API error") #restore
+            restore = True
+            altAPI = True
+        else:
+            if compare == True: newData = data #sets new data to latest data for comparison
+            try:
+                formattedData = json.loads(str(data)) #json decode
+            except:
+                restore = True
+                altAPI = True
+    if restore == True:
+        #if restore was enabled previously
+        with open(country + '.bak', 'r+') as src, open(country, 'w+') as dst:
+            dst.write(src.read()) #restore backup
+        with open(country, "r") as dataFile: #Re read restored data
+            data = dataFile.read()
+            if compare == True: newData = data #sets newData to restored value
+            formattedData = json.loads(str(data)) #json decode
+    data = formattedData #sets data variable to json decoded
+    totalCases = data['cases']
+    todayCases = data['todayCases']
+    totalDeath = data['deaths']
+    todayDeath = data['todayDeaths']
+    recovered = data['recovered']
+    critical = data['critical']
+    active = data['active']
+    fatalityRate = totalDeath / totalCases * 100
+    fatalityRatePercentage = round(fatalityRate, 2)
+    fatalityRateRounded = round(fatalityRate,0)
+    fatalityRatio = 100 - fatalityRateRounded
+    fatalityRatio = int(fatalityRatio)
+    composedMessage = ("{customizeBegin}COVID-19 Updates{linebreak}Total cases: "+str(totalCases)+"{}Cases today: "+str(todayCases)+"{}Critical cases: "+str(critical)+"{}Recovered: "+str(recovered)+"{}Total deaths: "+str(totalDeath)+"{}Deaths today: "+str(todayDeath)+"{}Current fatality rate: "+str(fatalityRatePercentage)+"%{}"+str(fatalityRatio)+" in 100 people will live based on current statistics{customizeEnd}")
+    if compare == True:
+        composedMessage = messageFormat(composedMessage, inputType, compare, data, oldData, newData)
+    elif compare == False:
+        composedMessage = messageFormat(composedMessage, inputType)
+    return composedMessage
+
+def getWorld(inputType, altAPI = False):
+    restore = False
+    if altAPI == True: 
+        url = 'https://corona.lmao.ninja/all'
+    elif altAPI == False:  
+        url = 'https://corona.lmao.ninja/all/'
+    r = requests.get(url)
+    try:
+        with open('world', 'r+') as src, open('world' + '.bak', 'w+') as dst:
+            dst.write(src.read()) #Backup
+    except:
+        pass
+    with open('world', 'wb') as outfile:
+        outfile.write(r.content) #Get latest and save
+    with open('world', "r+") as dataFile:
+        data = dataFile.read() #sets data to latest data
+        if data == 'Country not found': #if not available
+            print("API error") #restore
+            restore = True
+            altAPI = True
+        else:
+            try:
+                formattedData = json.loads(str(data)) #json decode
+            except:
+                restore = True
+                altAPI = True
+    if restore == True:
+        #if restore was enabled previously
+        with open('world' + '.bak', 'r+') as src, open('world', 'w+') as dst:
+            dst.write(src.read()) #restore backup
+        with open('world', "r") as dataFile: #Re read restored data
+            data = dataFile.read()
+            formattedData = json.loads(str(data)) #json decode
+    data = formattedData #sets data variable to json decoded
+    cases = data['cases']
+    death = data['deaths']
+    recovered = data['recovered']
+    fatalityRate = death / cases * 100
+    fatalityRatePercentage = round(fatalityRate, 2)
+    fatalityRateRounded = round(fatalityRate,0)
+    fatalityRatio = 100 - fatalityRateRounded
+    fatalityRatio = int(fatalityRatio)
+    composedMessage = "COVID-19 Worldwide Data{linebreak}Total cases: "+str(cases)+"{}Total deaths: "+str(death)+"{}Total recovered: "+str(recovered)+"{}Current fatality rate: "+str(fatalityRatePercentage)+"%{}"+str(fatalityRatio)+" in 100 people will live based on current statistics{customizeEnd}"
+    composedMessage = messageFormat(composedMessage, inputType)
+    return composedMessage
---
+++ main.py
@@ -0,0 +1,138 @@
+__author__ = "Ian Chan"
+__copyright__ = "Copyright 2020, Ian Chan"
+__credits__ = ["Ian Chan"]
+__license__ = "GPL v3"
+__version__ = "1"
+__maintainer__ = "Ian Chan"
+__email__ = "me@ian-chan.me"
+__status__ = "Production"
+
+from flask import Flask, request, Response
+from twilio.twiml.voice_response import VoiceResponse
+from twilio.twiml.messaging_response import MessagingResponse
+import covid19
+from twilio.rest import Client
+import os, time
+from multiprocessing import Process
+
+import configparser
+
+config = configparser.ConfigParser()
+
+if os.path.isfile('config.ini'):
+    pass
+else:
+    config['Settings'] = {'Twilio SID': '',
+                      'Authentication Token': '',
+                      'Phone Numbers': '+18005555551, +18005555552',
+                      'From Number' : '+8005555555',
+                      'Country': 'US',
+                      }
+    
+    with open('config.ini', 'w+') as configfile:
+        config.write(configfile)
+    
+    print("Configuration file created")
+    print("Edit configuration file then run again")
+    print("Press ENTER key to exit")
+    input()
+    exit()
+
+config.read('config.ini')
+
+settings = config['Settings']
+sid = settings.get('Twilio SID')
+auth = settings.get('Authentication Token')
+fromNumber = settings.get('From Number')
+phoneNumbers = settings.get('Phone Numbers')
+phoneNumbers = phoneNumbers.split(',')
+phoneNumbers = [x.strip(' ') for x in phoneNumbers]
+country = settings.get('Country')
+country = country.lower()
+altAPI = False #Does nothing
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return(covid19.getData('page', country, False, altAPI))
+
+def update():
+    while True:
+        time.sleep(60)
+        if covid19.getData('message', country, True, altAPI) == 'No Change':
+            pass
+        else:
+            config.read('config.ini')
+            phoneNumbers = settings.get('Phone Numbers')
+            phoneNumbers = phoneNumbers.split(',')
+            phoneNumbers = [x.strip(' ') for x in phoneNumbers] 
+            client = Client(sid, auth)
+            for currentNumber in phoneNumbers:
+                print("Sending message to: "+currentNumber)
+            message = client.messages.create(
+            to=currentNumber,
+            from_= fromNumber,
+            body=covid19.getData('message', country, False, altAPI))
+            print(message.sid)
+        #return(covid19.getData('page', country, False, altAPI))
+
+
+    
+@app.route('/config.ini')
+def block():
+  return Response(status=403)
+
+@app.route('/sms', methods=['POST'])
+def sms():
+    print("Received text")
+    resp = MessagingResponse()
+    body = request.values.get('Body', None)
+    body = body.lower()
+
+    if ('hi' in body) or ('hello' in body) or ('help' in body):
+        resp.message("Hello! Thanks for your interest in keeping updated with COVID-19 statistics in Canada. Ask me for the latest covid-19 cases or to give you an update, and you will receive the latest statistics.")
+    elif ('update' in body) or ('latest' in body):
+        resp.message(covid19.getData('message', country, False, altAPI))
+
+    elif ('thank' in body):
+        resp.message("No problem!")
+
+    elif 'stop' in body:
+        print('working')
+        config.read('config.ini')
+        phoneNumbers = settings.get('Phone Numbers')
+        numberToRemove = request.values.get('From', None)
+        newPhone = phoneNumbers.replace(", "+numberToRemove, "")
+        config.set('Settings', 'Phone Numbers', newPhone)
+        with open('config.ini', 'w') as configfile:
+            config.write(configfile)
+        resp.message("Remove from list")
+    
+    elif 'start' in body:
+        print('Subscribing...')
+        config.read('config.ini')
+        phoneNumbers = settings.get('Phone Numbers')
+        numberToAdd = request.values.get('From', None)
+        newPhone = phoneNumbers + ', '+numberToAdd
+        config.set('Settings', 'Phone Numbers', newPhone)
+        with open('config.ini', 'w') as configfile:
+            config.write(configfile)
+    elif ('world' in body):
+        resp.message(covid19.getWorld('message', altAPI))
+    return str(resp)
+
+
+
+@app.route('/call', methods=['POST'])
+def call():
+    print("Received call")
+    resp = VoiceResponse()
+    resp.say(covid19.getData('call', country, False, altAPI))
+    return str(resp)
+
+if __name__ == "__main__":
+    Process(target=update).start()
+    app.run(host='0.0.0.0', port='8080',debug=False)
